14009ce2bedef715059a9c57f1cc9ea564e10d6a
thiagocifani <cifani.thiago@gmail.com>
2019-11-11 13:45:58 +0200

    Yarn.lock

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+
+const routes = new Router();
+
+routes.post('/users', UserController.create());
+
+export default routes;
